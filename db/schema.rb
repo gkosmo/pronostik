8ff66ce0f7681447a147a4cb18b5b44a11cbbcfa
sean ariel <seanariel@hotmail.be>
2016-08-22 19:22:58 +0200

    Add url to bets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822160734) do
+ActiveRecord::Schema.define(version: 20160822171856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160822160734) do
     t.text     "justification"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "Url"
     t.index ["scenario_id"], name: "index_bets_on_scenario_id", using: :btree
     t.index ["user_id"], name: "index_bets_on_user_id", using: :btree
   end

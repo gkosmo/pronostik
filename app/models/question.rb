47108a2cd9afad9fa6a1d585cca3a252b98f54a5
sean ariel <seanariel@hotmail.be>
2016-08-23 12:13:04 +0200

    start show method add bet and scenario controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  has_many :scenarios
+  has_many :scenarios, dependent: :destroy
   has_and_belongs_to_many :questions
 
   belongs_to :user
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :questions
+  resources :questions do
+    resources :scenarios, only: [:new, :create] do
+      resources :bets, only: [:new, :create]
+    end
+  end
 end

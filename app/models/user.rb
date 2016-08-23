1a720ac6dccad4ad77ae482b467c9679c8a28925
sean ariel <seanariel@hotmail.be>
2016-08-23 09:49:53 +0200

    link models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
+  has_many :statistics
+  has_many :questions
+  has_many :just_upvotes
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.to_h.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)

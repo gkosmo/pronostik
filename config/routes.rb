8b7a09ea0fd935a106e0f6814429ac600e38c41a
george kosmopoulos <gkosmo1@hotmail.com>
2016-08-30 14:59:31 +0200

    done with country registration'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations', users: 'users#update' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
   root to: 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -23,6 +23,6 @@ Rails.application.routes.draw do
       resources :bets
       resources :notifications
       get 'history', to: 'histories#index'
-      get 'update', to: 'users#update'
+      put 'users' => 'devise/registrations#update', :as => 'user_registration' 
     end
 end
